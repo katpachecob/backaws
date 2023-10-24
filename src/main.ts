e7caeac73b7f1a063112a6d69b90f025ec4bd86b
KatherinePacheco <89138370+katpachecob@users.noreply.github.com>
2023-10-23 23:30:21 -0300

    update cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import cors from 'cors'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors()
+  const app = await NestFactory.create(AppModule,{cors:true});
+
   app.setGlobalPrefix('api/v1')
   app.useGlobalPipes(
     new ValidationPipe({
