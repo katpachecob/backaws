1a1ae2f82abdd6284d358f016aa115f57956eaea
KatherinePacheco <89138370+katpachecob@users.noreply.github.com>
2023-10-25 11:43:50 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -9,19 +9,26 @@ async function bootstrap() {
     rejectUnauthorized: false
   })
   app.enableCors({
-    allowedHeaders: ['Content-Type, Authorization'],
-    preflightContinue: false,
-    origin:'*',
-    methods: ["GET", "POST", "PATCH", "PUT"],
+    allowedHeaders: ['content-type'],
+    origin: [
+      "http://localhost:3000",
+      "https://localhost:3000",
+      "https://front-devacas.vercel.app",
+      "http://front-devacas.vercel.app",
+      "https://front-devacas-git-main-katpachecob.vercel.app",
+      "https://front-devacas-26e67tv5f-katpachecob.vercel.app"
+
+    ],
+    methods: ["GET", "POST", "PATCH", "PUT","OPTIONS", "HEAD", "DELETE"],
     credentials: true,
   })
 
   app.setGlobalPrefix('api/v1')
-  // app.useGlobalPipes(
-  //   new ValidationPipe({
-  //     whitelist: true
-  //   })
-  // )
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true
+    })
+  )
   await app.listen(8000, () => console.log(`Running in port 8000`));
 }
 bootstrap();
