6b0b23c0817b276ab7c578e2f4ff6a23f16609a5
KatherinePacheco <89138370+katpachecob@users.noreply.github.com>
2023-10-25 11:23:29 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as https from 'https'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  const httpsAgent = new https.Agent({
+    rejectUnauthorized: false
+  })
   app.enableCors({
     allowedHeaders: ['content-type'],
     origin: [
       "http://localhost:3000",
       "https://localhost:3000",
       "https://front-devacas.vercel.app",
-      "http://front-devacas.vercel.app"
+      "http://front-devacas.vercel.app",
+      "https://front-devacas-git-main-katpachecob.vercel.app",
+      "https://front-devacas-26e67tv5f-katpachecob.vercel.app"
+
     ],
     methods: ["GET", "POST", "PATCH", "PUT"],
     // credentials: true,
   })
+
   app.setGlobalPrefix('api/v1')
   app.useGlobalPipes(
     new ValidationPipe({
